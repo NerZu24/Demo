72d3ffaacc7bce551a273eff1d867f960b8baaec
NerZu24 <davidmegias524@outlook.es>
2024-05-16 14:43:14 +0200

    Create Test.ps1

---
+++ Test.ps1
@@ -0,0 +1,11 @@
+echo "Hello world"
+
+$name="test"
+
+if ($name=$name1){
+mkdir C:\ProgramData\Jenkins\.jenkins\workspace\test\$name1.txt
+
+Send-MailMessage -From "nerzutraga23@gmail.com" -To "nerzutraga23@gmail.com" -Body "This is the example" -Subject "Jenkins Check" -SmtpServer smtp-relay.gmail.com
+
+rmdir C:\ProgramData\Jenkins\.jenkins\workspace\test\$name.txt
+}
